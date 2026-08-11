1db4054e4369d1f726ccf2cff43c37a4d8cdcd4b
Shucayb Ayuub <shucayb_Ayuub>
2026-08-11 09:06:19 +0300

    M

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "expenses-trackers-ten.vercel.app", credentials: true }));
+app.use(cors({ origin: "https://expenses-trackers-ten.vercel.app", credentials: true }));
 
 app.get("/", (req, res) => {
     res.send("API WORKING");
